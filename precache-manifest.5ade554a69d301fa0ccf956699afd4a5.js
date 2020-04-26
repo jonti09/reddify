d09dec139875d90a7bd129ab775fd60b5c3326e1
jonti <jonti.savani@vphrase.com>
2020-04-26 16:13:30 +0530

    Updates

---
+++ precache-manifest.5ade554a69d301fa0ccf956699afd4a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e581e4693b139ea03bf7483677bdb8e",
+    "url": "/reddify/index.html"
+  },
+  {
+    "revision": "150101095c4e14b0ce79",
+    "url": "/reddify/static/js/2.c791debc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reddify/static/js/2.c791debc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4308b2c3a2e72721ea3",
+    "url": "/reddify/static/js/main.b76aa7ab.chunk.js"
+  },
+  {
+    "revision": "ecb5dd086d1646b62c95",
+    "url": "/reddify/static/js/runtime-main.c0053636.js"
+  }
+]);
