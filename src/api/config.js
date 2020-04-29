42d2128d7081470381072f72bfdc647709454e81
jonti <jonti.savani@vphrase.com>
2020-04-29 10:52:17 +0530

    - added config to respect the env

--- src/api/config.js
+++ src/api/config.js
@@ -7,12 +7,31 @@ const getUUID = () => {
   });
 };
 
+const getEnvVar = (key) => {
+  let modeString = {
+    development: {
+      REACT_APP_REDDIT_REDIRECT: process.env.REACT_APP_REDDIT_DEV_REDIRECT,
+      REACT_APP_REDDIT_CLIENT_ID: process.env.REACT_APP_REDDIT_DEV_CLIENT_ID,
+      REACT_APP_REDDIT_CLIENT_SECRET: process.env.REACT_APP_REDDIT_DEV_CLIENT_SECRET,
+    },
+    production: {
+      REACT_APP_REDDIT_REDIRECT: process.env.REACT_APP_REDDIT_PROD_REDIRECT,
+      REACT_APP_REDDIT_CLIENT_ID: process.env.REACT_APP_REDDIT_PROD_CLIENT_ID,
+      REACT_APP_REDDIT_CLIENT_SECRET: process.env.REACT_APP_REDDIT_PROD_CLIENT_SECRET,
+    },
+  };
+
+  return modeString[process.env.NODE_ENV][key];
+};
+
 export const redditConfig = {
   SCOPE: `identity,edit,flair,history,modconfig,modflair,modlog,modposts,modwiki,mysubreddits,privatemessages,read,report,save,submit,subscribe,vote,wikiedit,wikiread`,
-  REDIRECT_URI: process.env.REACT_APP_REDDIT_REDIRECT,
-  CLIENT_ID: process.env.REACT_APP_REDDIT_CLIENT_ID,
+  REDIRECT_URI: getEnvVar('REACT_APP_REDDIT_REDIRECT'),
+  CLIENT_ID: getEnvVar('REACT_APP_REDDIT_CLIENT_ID'),
   RESPONSE_TYPE: 'code',
   STATE: getUUID(),
   DURATION: 'permanent',
-  CLIENT_SECRET: process.env.REACT_APP_REDDIT_CLIENT_SECRET,
+  CLIENT_SECRET: getEnvVar('REACT_APP_REDDIT_CLIENT_SECRET'),
 };
+
+console.log(redditConfig)
